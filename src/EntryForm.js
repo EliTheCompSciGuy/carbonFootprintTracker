1a3531318feb89da0daf1af7c74d9568c48e5657
EliTheCompSciGuy <35388026+EliTheCompSciGuy@users.noreply.github.com>
2023-06-09 12:37:15 -0400

    Calculations

--- src/EntryForm.js
+++ src/EntryForm.js
@@ -17,34 +17,63 @@ function EntryForm() {
     const [waterHome, setWaterHome] = useState(0)
     const [numInHousehold, setNumInHousehold] = useState(0)
 
-    //calc constatnts, converted everything into grams
+    //const [isLoading, setIsLoading] = useState(false)
+    //const [errorMessage, setErrorMessage] = useState('')
+    //const results = []
+
+    //calc constatnts, converted food into grams
     const avgCO2perCal_meat = .01245
     const avgCO2perCal_dairy = .00488
     const avgCO2perCal_produce = .00115
     const avgCO2perCal_carb = .00069
-    const avgCO2perGal_gasCar = 8887
-    const avgCO2perGal_dieselCar = 10180
-    const avgCO2perMile_bus = 140.885
-    const avgCO2perMile_train = 40.641
-    const avgCO2perMile_plane = 181.437
-    const avgCO2perkWh_elec = 356.432
-    const avgCO2perkWh_gas = 165.561
-    const avgCO2perGal_water = 10.5
 
+    //calc constatnts, converted everything else into kilograms
+    const avgCO2perGal_gasCar = 8887 / 1000
+    const avgCO2perGal_dieselCar = 10180 / 1000
+    const avgCO2perMile_bus = 140.885 / 1000
+    const avgCO2perMile_train = 40.641 / 1000
+    const avgCO2perMile_plane = 181.437 / 1000
+    const avgCO2perkWh_elec = 356.432 / 1000
+    const avgCO2perkWh_gas = 165.561 / 1000
+    const avgCO2perGal_water = 10.5 / 1000
+
+    /*
     const handleSubmit = async (e) => {
-        e.preventDefault()
-        if (!isValidInputs()) {
-            return
-        }
-    }
+      e.preventDefault()
+      try{
+        setIsLoading(true)
+        calculateYearlyResults()
+      }
 
-    const isValidInputs = (attr) => {
-        return true;
-    }
+      catch (error) {
+        setErrorMessage('Please make sure you enter in positive numbers.')
+      }
 
+      finally {
+        setIsLoading(false)
+      }
+    }
+    */
+
+    /*
+    const calculateYearlyResults = () => {
+      results[0] = meatCal * avgCO2perCal_meat * 365
+      results[1] = dairyCal * avgCO2perCal_dairy * 365
+      results[2] = produceCal * avgCO2perCal_produce * 365
+      results[3] = carbCal * avgCO2perCal_carb * 365
+      results[4] = (gasCar * avgCO2perGal_gasCar) / carMileage
+      results[5] = (dieselCar * avgCO2perGal_dieselCar) / carMileage
+      results[6] = bus * avgCO2perMile_bus
+      results[7] = train * avgCO2perMile_train
+      results[8] = plane * avgCO2perMile_plane
+      results[9] = (electrictyHome * avgCO2perkWh_elec) / numInHousehold
+      results[10] = (gasHome * avgCO2perkWh_gas) / numInHousehold
+      results[11] = (waterHome * avgCO2perGal_water) / numInHousehold 
+    }
+    */
     return (
         <div className="form-container" style={{ marginBottom: '80px' }}>
-            <form onSubmit={handleSubmit}>
+            <form>
                 <p>Food:</p>
                 <div>
                   <label className="block">
@@ -90,7 +119,7 @@ function EntryForm() {
                 <p>Travel:</p>
                 <div>
                   <label className="block">
-                    <span className="standard-text">Enter your car mileage:</span>
+                    <span className="standard-text">Enter your car mileage per gallon:</span>
                       <input
                         type="num"
                         value={carMileage}
@@ -191,12 +220,25 @@ function EntryForm() {
                   </label>
                 </div>
 
-                <button
-                type="submit" >
-                Calculate
-                </button>
+                <div>
+                  <p>Food:</p>
+                  <p>Your yearly meat CO2 emissions: {(meatCal * avgCO2perCal_meat * 365).toFixed(2)}g</p>
+                  <p>Your yearly dairy CO2 emissions: {(dairyCal * avgCO2perCal_dairy * 365).toFixed(2)}g</p>
+                  <p>Your yearly produce CO2 emissions: {(produceCal * avgCO2perCal_produce * 365).toFixed(2)}g</p>
+                  <p>Your yearly carbs CO2 emissions: {(carbCal * avgCO2perCal_carb * 365).toFixed(2)}g</p>
+                  <p>Travel:</p>
+                  <p>Your yearly car CO2 emissions: {(((gasCar * avgCO2perGal_gasCar) / carMileage)+ ((dieselCar * avgCO2perGal_dieselCar) / carMileage)).toFixed(2)}kg</p>
+                  <p>Your yearly bus CO2 emissions: {(bus * avgCO2perMile_bus).toFixed(2)}kg</p>
+                  <p>Your yearly train CO2 emissions: {(train * avgCO2perMile_train).toFixed(2)}kg</p>
+                  <p>Your yearly plane CO2 emissions: {(plane * avgCO2perMile_plane).toFixed(2)}kg</p>
+                  <p>Home:</p>
+                  <p>Your yearly home electricity CO2 emissions: {((electrictyHome * avgCO2perkWh_elec) / numInHousehold).toFixed(2)}kg</p>
+                  <p>Your yearly home gas CO2 emissions: {((gasHome * avgCO2perkWh_gas) / numInHousehold).toFixed(2)}kg</p>
+                  <p>Your yearly home water CO2 emissions: {((waterHome * avgCO2perGal_water) / numInHousehold).toFixed(2)}kg</p>
+                </div>
             </form>
         </div>
+
     )
 }
 export default EntryForm;
